5254045a267afcc4b7262ccecb8ac8aaad054f78
EduardoFreitas <eduardo.freitas.cp@gmail.com>
2018-05-10 00:02:18 -0300

    first commit

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'/>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor</p>
+        <h2>Missão e valores</h2>
+        <p>Lorem ipsum dolor</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor</p>
+    </div>
+)
