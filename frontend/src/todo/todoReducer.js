d3b5494ab87a3269b5faf7e0e9f7579bfcc41a56
EduardoFreitas <eduardo.freitas.cp@gmail.com>
2018-05-19 22:55:34 -0300

    Midware

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,24 +1,16 @@
 const INITIAL_STATE = {
-    description: 'Ler livro',
-    list: [{
-        _id: 1,
-        description: 'Pagar',
-        done: true
-    }, {
-        _id: 2,
-        description: 'Reuniao',
-        done: false
-    }, {
-        _id: 3,
-        description: 'Consuta',
-        done: false
-    }]
+    description: '',
+    list: []
 }
 
 export default (state = INITIAL_STATE, action) =>{
     switch (action.type){
         case 'DESCRIPTION_CHANGE':
             return { ...state, description: action.payload}
+        case 'TODO_SEARCHED':
+            return { ...state, list: action.payload.data}
+        case 'TODO_ADD':
+            return { ...state, description: ''}
         default:
             return state
     }
